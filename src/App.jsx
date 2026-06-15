f9965a97f6bafd18233781785846ffd27ff5663b
Myltiane Alves <myltiane.aux@gmail.com>
2026-06-15 13:51:49 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+
+import { RoutesMain } from './routes/Routes';
+import { DashBoardGerencia } from './pages/DashBoardGerencia';
+
+function App() {
+  return (
+    <DashBoardGerencia />
+  )
+}
+
+export default App;
---
+++ src/pages/DashBoardGerencia.jsx
@@ -0,0 +1,59 @@
+import React, { Fragment, useEffect, useState, Suspense, lazy } from "react"
+import { useFetchData } from "../hooks/useFetchData";
+import { useQuery } from "react-query";
+
+
+
+const ActionPesquisaProdutoEtiqueta = lazy(() => import("../componets/Gerencia/Components/ActionProdutoEtiqueta/actionPesquisaProdutoEtiqueta").then(module => ({ default: module.ActionPesquisaProdutoEtiqueta })));
+
+export const DashBoardGerencia = () => {
+  const [componentToShow, setComponentToShow] = useState("");
+
+
+  let component = null;
+
+  switch (componentToShow) {
+    case "/gerencia/ActionPesquisaProdutoEtiqueta":
+      component = <ActionPesquisaProdutoEtiqueta />;
+      break;
+    default:
+      component = null;
+      break;
+  }
+
+  return (
+
+    <Fragment>
+
+      <div className="page-wrapper">
+        <div className="page-inner">
+        
+          <div className="page-content-wrapper">
+            
+            <main id="js-page-content" role="main" className="page-content">
+              <div className="row">
+                <div className="col-xl-12">
+                  <div id="panel-1" className="panel">
+                    <div className="panel-container show">
+                      <div className="panel-content">
+                        <Suspense fallback={<div>Loading...</div>}>
+                        
+                          <ActionPesquisaProdutoEtiqueta />
+
+                          
+                        </Suspense>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+
+            
+          </div>
+        </div>
+      </div>
+            
+    </Fragment>
+  )
+}
