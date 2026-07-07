23b469028e419c3cea564ffdea70baf54698b98c
Myltiane Alves <myltiane.aux@gmail.com>
2026-07-07 16:14:45 -0300

    refactor: etiquetas produtos

--- src/componets/Gerencia/Components/ActionProdutoEtiqueta/actionDetalharProdutosEtiquetaModal.jsx
+++ src/componets/Gerencia/Components/ActionProdutoEtiqueta/actionDetalharProdutosEtiquetaModal.jsx
@@ -239,7 +239,7 @@ export const ActionDetalharProdutosEtiquetaModal = ({
                   <h2>LISTA DE PRODUTOS PARA IMPRIMIR</h2>
                 </div>
 
-                <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+                {/* <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
                   <HeaderTable
                     globalFilterValue={globalFilterValue}
                     onGlobalFilterChange={onGlobalFilterChange}
@@ -247,7 +247,7 @@ export const ActionDetalharProdutosEtiquetaModal = ({
                     exportToExcel={exportToExcel}
                     exportToPDF={exportToPDF}
                   />
-                </div>
+                </div> */}
                 <div className="card" ref={dataTableRef}>
                   <DataTable
                     value={dados}
